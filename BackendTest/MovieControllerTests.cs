5cbf97db1e0e3b294031dd1b9e984fd2d7ff10fc
Plat00n <70469468+DefinitlyNotABot@users.noreply.github.com>
2025-05-26 10:04:28 +0200

    Added tests

---
+++ BackendTest/MovieControllerTests.cs
@@ -0,0 +1,68 @@
+﻿using Xunit;
+using Moq;
+using API.Controllers;
+using API.Services;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using API.OutputDto;
+
+public class MovieControllerTests
+{
+    private readonly Mock<IMovieService> _movieServiceMock;
+    private readonly MovieController _controller;
+
+    public MovieControllerTests()
+    {
+        _movieServiceMock = new Mock<IMovieService>();
+        _controller = new MovieController(_movieServiceMock.Object);
+    }
+
+    [Fact]
+    public async Task GetAllMovies_ReturnsOkWithMovies()
+    {
+        _movieServiceMock.Setup(s => s.GetAllMovies())
+            .ReturnsAsync(new List<MovieDto> { new MovieDto() });
+
+        var result = await _controller.GetAllMovies();
+
+        var okResult = Assert.IsType<OkObjectResult>(result);
+        Assert.IsType<List<MovieDto>>(okResult.Value);
+    }
+
+    [Fact]
+    public async Task GetAllMovies_ReturnsNotFound_WhenNull()
+    {
+        _movieServiceMock.Setup(s => s.GetAllMovies())
+            .ReturnsAsync((List<MovieDto>?)null);
+
+        var result = await _controller.GetAllMovies();
+
+        var notFoundResult = Assert.IsType<NotFoundObjectResult>(result);
+        Assert.Equal("No movies found!", notFoundResult.Value);
+    }
+
+    [Fact]
+    public async Task GetMovieById_ReturnsOkWithMovie()
+    {
+        _movieServiceMock.Setup(s => s.GetMovieById(1))
+            .ReturnsAsync(new MovieDto());
+
+        var result = await _controller.GetMovieById(1);
+
+        var okResult = Assert.IsType<OkObjectResult>(result);
+        Assert.IsType<MovieDto>(okResult.Value);
+    }
+
+    [Fact]
+    public async Task GetMovieById_ReturnsOkWithMinusOne_WhenNull()
+    {
+        _movieServiceMock.Setup(s => s.GetMovieById(1))
+            .ReturnsAsync((MovieDto?)null);
+
+        var result = await _controller.GetMovieById(1);
+
+        var okResult = Assert.IsType<OkObjectResult>(result);
+        Assert.Equal(-1, okResult.Value);
+    }
+}
---
+++ BackendTest/UserControllerTests.cs
@@ -0,0 +1,72 @@
+﻿using API.OutputDto;
+
+namespace BackendTest;
+
+using Xunit;
+using Moq;
+using API.Controllers;
+using API.Services;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using API.Models;
+
+public class UserControllerTests
+{
+    private readonly Mock<IUserService> _userServiceMock;
+    private readonly UserController _controller;
+
+    public UserControllerTests()
+    {
+        _userServiceMock = new Mock<IUserService>();
+        _controller = new UserController(_userServiceMock.Object);
+    }
+
+    [Fact]
+    public async Task GetAllUsers_ReturnsOkWithUsers()
+    {
+        _userServiceMock.Setup(s => s.GetAllUsersAsync())
+            .ReturnsAsync(new List<UserDto> { new UserDto() });
+
+        var result = await _controller.GetAllUsers();
+
+        var okResult = Assert.IsType<OkObjectResult>(result);
+        Assert.IsType<List<UserDto>>(okResult.Value);
+    }
+
+    [Fact]
+    public async Task GetAllUsers_ReturnsOkWithMinusOne_WhenNull()
+    {
+        _userServiceMock.Setup(s => s.GetAllUsersAsync())
+            .ReturnsAsync((List<UserDto>?)null);
+
+        var result = await _controller.GetAllUsers();
+
+        var okResult = Assert.IsType<OkObjectResult>(result);
+        Assert.Equal(-1, okResult.Value);
+    }
+
+    [Fact]
+    public async Task GetUserById_ReturnsOkWithUser()
+    {
+        _userServiceMock.Setup(s => s.GetUserById(1))
+            .ReturnsAsync(new UserDto());
+
+        var result = await _controller.GetUserById(1);
+
+        var okResult = Assert.IsType<OkObjectResult>(result);
+        Assert.IsType<UserDto>(okResult.Value);
+    }
+
+    [Fact]
+    public async Task GetUserById_ReturnsOkWithMinusOne_WhenNull()
+    {
+        _userServiceMock.Setup(s => s.GetUserById(1))
+            .ReturnsAsync((UserDto)null);
+
+        var result = await _controller.GetUserById(1);
+
+        var okResult = Assert.IsType<OkObjectResult>(result);
+        Assert.Equal(-1, okResult.Value);
+    }
+}
